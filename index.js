5d89e1275bd18444dc75ca4bfe8a0a89148e6eab
Vinit Miranda <vmiranda@connect.hku.hk>
2023-09-08 19:45:40 +0800

    UDP Bridge working with vcv

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const maxApi = require('max-api');
 const WebSocket = require('ws');
-const ws = new WebSocket('wss://5f7a-182-239-87-197.ngrok-free.app:443');
+const ws = new WebSocket('wss://tangosocket.onrender.com:443');
 
 ws.on('message', (message) => {
     maxApi.outlet(parseFloat(message))
